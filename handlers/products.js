1e69e511230c92b23bfc6ab2a0c24995da2d66a1
Aaron Macken <aaron.a.macken@gmail.com>
2019-12-16 21:41:58 -0800

    Create the update route and function

--- handlers/products.js
+++ handlers/products.js
@@ -35,6 +35,16 @@ exports.getProduct = async function(req, res, next) {
     }
 }
 
+exports.updateProduct = async function(req, res, next) {
+    try {
+        // find DB document by req.params.product_id, update JSON with req.body, return updated object via json
+        let foundProductUpdate = await db.Product.findOneAndUpdate({_id: req.params.product_id}, req.body, {new: true});
+        return res.status(200).json(foundProductUpdate);
+    } catch(err) {
+        return next(err);
+    }
+}
+
 exports.deleteProduct = async function(req, res, next) {
     try {
         let foundProductDelete = await db.Product.findById(req.params.product_id);
