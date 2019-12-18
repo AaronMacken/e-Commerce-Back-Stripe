79ac268fd6b89433c573a99342303aeb186388e1
Aaron Macken <aaron.a.macken@gmail.com>
2019-12-17 21:39:32 -0800

    properly functioning JWT and Bcrypt auth. Re-enabled the signin route

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,6 @@ const {signup, signin} = require("../handlers/admin");
 
 router.post("/signup", signup);
 
-// router.post("/signin", signin)
+router.post("/signin", signin);
 
 module.exports = router;
