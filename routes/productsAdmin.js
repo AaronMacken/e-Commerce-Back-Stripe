231033ee1d20587dec26b6b0be968b9550ca1cc4
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-15 21:05:56 -0800

    Debugging multer upload path availability

--- routes/productsAdmin.js
+++ routes/productsAdmin.js
@@ -4,7 +4,8 @@ const router = express.Router({ mergeParams: true });
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, './uploads/');
+    // cb(null, './uploads/');
+    cb(null,path.join(__dirname+'/uploads'));
   },
   filename: function(req, file, cb) {
       cb(null, file.originalname)
