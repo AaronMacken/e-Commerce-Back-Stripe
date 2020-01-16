916e866a6b91d27f9aea42baf4acae022e604b02
Aaron Macken <aaron.a.macken@gmail.com>
2020-01-15 21:15:31 -0800

    Debugging...

--- routes/productsAdmin.js
+++ routes/productsAdmin.js
@@ -4,7 +4,8 @@ const router = express.Router({ mergeParams: true });
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, path.join(__dirname, '/uploads/'));
+    cb(null, '/uploads/');
+    
   },
   filename: function(req, file, cb) {
       cb(null, file.originalname)
